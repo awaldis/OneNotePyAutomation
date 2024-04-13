39e08848f8c4837229a70da7b0038825312a7060
Andrew Waldis <andrew@andrewwaldis.com>
2024-04-13 13:54:26 -0400

    Convert to using object methods instead of freee functions

--- onenote_auto.py
+++ onenote_auto.py
@@ -3,109 +3,90 @@ import requests
 import yaml
 
 
-def get_access_token(config_filename="config.yaml"):
-    with open(config_filename) as config_file:
-        config = yaml.safe_load(config_file)
+class OneNoteClient:
+    def __init__(self, config_filename="config.yaml"):
+        self.config_filename = config_filename
+        self.access_token = self.get_access_token()
+        if self.access_token is None:
+            raise Exception("Failed to obtain access token.")
 
-    # Initialize the MSAL public client
-    authority = "https://login.microsoftonline.com/consumers"
-    app = msal.PublicClientApplication(
-        config["client_id"],
-        authority=authority,
-    )
+    def get_access_token(self):
+        with open(self.config_filename) as config_file:
+            config = yaml.safe_load(config_file)
 
-    # Acquire a token to access Microsoft Graph
-    scope = ["Notes.ReadWrite"]
-    token_response = app.acquire_token_interactive(scopes=scope)
-
-    if "access_token" in token_response:
-        return token_response["access_token"]
-
-    # If we get here then we failed to get an access token.
-    return None
-
-
-def get_notebook_names(access_token):
-    # Create HTTP GET request header
-    headers = {
-        "Authorization": "Bearer " + access_token,
-    }
-
-    url = "https://graph.microsoft.com/v1.0/me/onenote/notebooks?$select=id,displayName"
-
-    return requests.get(url, headers=headers)
-
-
-def get_section_names(access_token, notebook_id):
-    # Create HTTP GET request header
-    headers = {
-        "Authorization": "Bearer " + access_token,
-    }
-    url = f"https://graph.microsoft.com/v1.0/me/onenote/notebooks/{notebook_id}/sections?$select=id,displayName"
-
-    return requests.get(url, headers=headers)
-
-
-def list_notebook_names(access_token):
-    response = get_notebook_names(access_token)
-    response_json = response.json()
-    for i in range(len(response_json["value"])):
-        print(response_json["value"][i]["displayName"])
-
-
-def get_notebook_id(access_token, notebook_name):
-    response = get_notebook_names(access_token)
-    response_json = response.json()
-
-    for i in range(len(response_json["value"])):
-        if response_json["value"][i]["displayName"] == notebook_name:
-            return response_json["value"][i]["id"]
-
-    print(f"Error - Could not find notebook {notebook_name}.")
-    return ()
-
-
-def add_notebook_pages(
-    access_token, page_title_string_list, config_filename="config.yaml"
-):
-    # TODO get rid of this file read.
-    with open(config_filename) as config_file:
-        config = yaml.safe_load(config_file)
-
-    section_id = config["section_id"]
-
-    # HTML template string
-    page_content_template = """
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>{}</title>
-        </head>
-        <body>
-        </body>
-        </html>
-    """
-    # Create HTTP POST request headers
-    headers = {
-        "Authorization": "Bearer " + access_token,
-        "Content-Type": "application/xhtml+xml",
-    }
-    # One loop for each title in the title list
-    for title in page_title_string_list:
-        # Insert the title into the HTML
-        final_page_content = page_content_template.format(title)
-
-        # Now post the request
-        url = f"https://graph.microsoft.com/v1.0/me/onenote/sections/{section_id}/pages"
-        response = requests.post(
-            url, headers=headers, data=final_page_content.encode("utf-8")
+        # Initialize the MSAL public client
+        authority = "https://login.microsoftonline.com/consumers"
+        app = msal.PublicClientApplication(
+            config["client_id"],
+            authority=authority,
         )
 
-        if response.status_code == requests.codes.created:
-            print("Page with title " + '"' + title + '" created successfully.')
-        else:
-            print("ERROR! - Could not create page with title " + '"' + title + '" .')
-            print(response.status_code)
-            print(response.json())
-            print("No further page creations will be attempted.")
-            break
+        # Acquire a token to access Microsoft Graph
+        scope = ["Notes.ReadWrite"]
+        token_response = app.acquire_token_interactive(scopes=scope)
+
+        return token_response.get("access_token")
+
+    def get_notebook_names(self):
+        headers = {"Authorization": "Bearer " + self.access_token}
+        url = "https://graph.microsoft.com/v1.0/me/onenote/notebooks?$select=id,displayName"
+        return requests.get(url, headers=headers)
+
+    def get_section_names(self, notebook_id):
+        headers = {"Authorization": "Bearer " + self.access_token}
+        url = f"https://graph.microsoft.com/v1.0/me/onenote/notebooks/{notebook_id}/sections?$select=id,displayName"
+        return requests.get(url, headers=headers)
+
+    def list_notebook_names(self):
+        response = self.get_notebook_names()
+        response_json = response.json()
+        for item in response_json["value"]:
+            print(item["displayName"])
+
+    def get_notebook_id(self, notebook_name):
+        response = self.get_notebook_names()
+        response_json = response.json()
+
+        for item in response_json["value"]:
+            if item["displayName"] == notebook_name:
+                return item["id"]
+
+        print(f"Error - Could not find notebook {notebook_name}.")
+        return None
+
+    def add_notebook_pages(self, page_title_string_list):
+        # Load section_id from config
+        with open(self.config_filename) as config_file:
+            config = yaml.safe_load(config_file)
+        section_id = config["section_id"]
+
+        page_content_template = """
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>{}</title>
+            </head>
+            <body>
+            </body>
+            </html>
+        """
+
+        headers = {
+            "Authorization": "Bearer " + self.access_token,
+            "Content-Type": "application/xhtml+xml",
+        }
+
+        for title in page_title_string_list:
+            final_page_content = page_content_template.format(title)
+            url = f"https://graph.microsoft.com/v1.0/me/onenote/sections/{section_id}/pages"
+            response = requests.post(
+                url, headers=headers, data=final_page_content.encode("utf-8")
+            )
+
+            if response.status_code == requests.codes.created:
+                print(f'Page with title "{title}" created successfully.')
+            else:
+                print(f'ERROR! - Could not create page with title "{title}".')
+                print(response.status_code)
+                print(response.json())
+                break
